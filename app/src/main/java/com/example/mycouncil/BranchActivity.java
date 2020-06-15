303d9de24a178b8e22bfd23beb178a2f8b21f1bf
Larry <larryzhi1230@gmail.com>
2020-06-15 11:43:26 -0700

    added buttons and labels

--- app/src/main/java/com/example/mycouncil/BranchActivity.java
+++ app/src/main/java/com/example/mycouncil/BranchActivity.java
@@ -1,15 +1,29 @@
 package com.example.mycouncil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BranchActivity extends AppCompatActivity {
 
+    Button mBack;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_branch);
+
+        mBack = findViewById(R.id.back);
+
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mycouncil/HomeActivity.java
+++ app/src/main/java/com/example/mycouncil/HomeActivity.java
@@ -1,15 +1,45 @@
 package com.example.mycouncil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button mLogout, mBranch, mCreate;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mLogout = findViewById(R.id.logout);
+        mBranch = findViewById(R.id.branch);
+        mCreate = findViewById(R.id.create);
+
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+        mBranch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BranchActivity.class));
+            }
+        });
+
+        mCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateActivity.class));
+            }
+        });
     }
 }
