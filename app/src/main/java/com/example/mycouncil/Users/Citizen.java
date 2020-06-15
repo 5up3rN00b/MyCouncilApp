2376ee7a044ddddd13b3872ab9929288be267564
5up3rN00b <aaron.linear@gmail.com>
2020-06-15 14:26:08 -0700

    Changed name

--- app/src/main/java/com/example/mycouncil/Users/Citizen.java
+++ app/src/main/java/com/example/mycouncil/Users/Citizen.java
@@ -4,10 +4,15 @@ public class Citizen implements User {
     private String firstName, lastName, email;
     private int zipCode;
 
-    public Citizen(String firstName, String lastName, int zipCode) {
+    public Citizen(String email, String firstName, String lastName, int zipCode) {
+        this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.zipCode = zipCode;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String getFirstName() {
