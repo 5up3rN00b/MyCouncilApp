33a2285bd8a54ed3057a6c695bea95b1f18293cc
Zheng Andrew <andrewzheng04@gmail.com>
2020-06-18 20:21:34 -0700

    idfk

--- app/src/main/java/com/example/mycouncil/HomeActivity.java
+++ app/src/main/java/com/example/mycouncil/HomeActivity.java
@@ -234,10 +234,6 @@ public class HomeActivity extends AppCompatActivity {
             String branchNameEnter = list.get(position).getBranch();
             Integer userID = list.get(position).getUserId();
 
-
-
-
-
             LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(postResource, parent, false);
 
@@ -318,13 +314,6 @@ public class HomeActivity extends AppCompatActivity {
 
                 }
             });
-
-
-
-
-
-
-
             return convertView;
         }
     }
